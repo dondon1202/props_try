7ef2904a30c056cdbfbe314e004ce159c08e4534
dondon <domi61503@gmail.com>
2023-05-07 19:50:30 +0800

    props陣列傳值

---
+++ src/components/TestItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ parentMsg }}</h1>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '內層msg'
+    }
+  },
+  props: ['parentMsg']
+}
+</script>
+
+<style></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <TestItem :parent-msg="msg" />
+</template>
+
+<script>
+import TestItem from '../components/TestItem.vue'
+export default {
+  data() {
+    return {
+      msg: '外面msg'
+    }
+  },
+  components: {
+    TestItem
+  }
+}
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
